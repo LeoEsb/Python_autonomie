b2b1fba0c346f5dc11a5d0e3bb92064567e6a8dc
LeoEsb <43401721+LeoEsb@users.noreply.github.com>
2019-04-28 22:40:40 +0200

    Add files via upload

---
+++ Conversion_nbr/conversion.py
@@ -0,0 +1,12 @@
+import json
+
+def conversion():
+    mon_fichier = open("conversion.json", "r")
+    contenu = mon_fichier.read() 
+    #print(contenu)
+
+
+nombre=input("Entrez un nombre en lettre : ")
+
+if nombre == conversion:
+    print("le nombre est '{1}' ", conversion)
